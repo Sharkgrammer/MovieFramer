5036e908f5ed8437287d3ad59761fb954ea51de5
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-08-17 23:23:14 +0100

    Fixed rounding error

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 from to_byte_arr import to_byte_arr, hacky_final_output
 from crop_image import crop_image
 
-video_path = 'shrek.mp4'
+video_path = 'test.mp4'
 output_folder = 'frames'
 desired_frames = 60 * 24
 
@@ -15,9 +15,12 @@ image_quality = 30
 should_crop_image = True
 crop_dimen = (200, 200)  # Cropped image width, height
 
-in_colour = False
+in_colour = True
 
-output_byte_arr = True
+output_as_image = True
+image_prefix = "frame_"
+
+output_byte_arr = False
 output_text_file = "frames.h"
 output_prefix = "f"
 
@@ -33,7 +36,8 @@ video = cv2.VideoCapture(video_path)
 print("Reading Metadata...")
 total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
-count = jump = int(total_frames / desired_frames)
+count = jump = total_frames / desired_frames
+
 video.set(cv2.CAP_PROP_POS_FRAMES, count)
 
 print(f"Total Frames:'{total_frames}' & Frame Interval:'{jump}'")
@@ -58,11 +62,11 @@ while video.isOpened():
         if should_crop_image:
             image = crop_image(image, image_dimen, crop_dimen)
 
-
         if output_byte_arr:
             to_byte_arr(image, crop_dimen if output_byte_arr else image_dimen, output_text_file, output_prefix)
-        else:
-            output_path = os.path.join(output_folder, f'frame_{count:04d}.jpg')
+
+        if output_as_image:
+            output_path = os.path.join(output_folder, f'{image_prefix}{int(count):04d}.jpg')
             image.save(output_path, quality=image_quality)
 
         count += jump
@@ -72,8 +76,7 @@ while video.isOpened():
 
         if time_checker > 10:
             time_checker = 0
-            print(f"Frame {count} of {total_frames}")
-
+            print(f"Frame {round(count, 2)} of {total_frames}")
 
     else:
         video.release()
